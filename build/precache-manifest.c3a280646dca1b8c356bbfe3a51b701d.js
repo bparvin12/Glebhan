24b69fc44d80fe3acb0dd0cdeed1e76c91a46f77
Bijan Parvin <bparvin12@gmail.com>
2019-08-18 20:02:26 -0700

    workng

---
+++ build/precache-manifest.c3a280646dca1b8c356bbfe3a51b701d.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "8827c855875c505d5619",
+    "url": "/Glebhan/static/css/main.53f3cff8.chunk.css"
+  },
+  {
+    "revision": "8827c855875c505d5619",
+    "url": "/Glebhan/static/js/main.0d3e8a58.chunk.js"
+  },
+  {
+    "revision": "b15cce8bf1410e9e75ce",
+    "url": "/Glebhan/static/js/runtime~main.1250bb39.js"
+  },
+  {
+    "revision": "6762c3da99fb8213d17a",
+    "url": "/Glebhan/static/css/2.93ec79a4.chunk.css"
+  },
+  {
+    "revision": "6762c3da99fb8213d17a",
+    "url": "/Glebhan/static/js/2.b57b1505.chunk.js"
+  },
+  {
+    "revision": "79737a3a9dcf6f5960ee0d7c3aae73ee",
+    "url": "/Glebhan/static/media/Cover Photo_.79737a3a.jpg"
+  },
+  {
+    "revision": "37bf2fd301fb6e56018e045157deab9d",
+    "url": "/Glebhan/static/media/Cover photo2_.37bf2fd3.jpg"
+  },
+  {
+    "revision": "8c9e88439b8d5a043f086133d09c54d7",
+    "url": "/Glebhan/static/media/pic1.8c9e8843.jpg"
+  },
+  {
+    "revision": "1bad1f0421d42dc1d1c153db0a40b21a",
+    "url": "/Glebhan/static/media/pic2.1bad1f04.jpg"
+  },
+  {
+    "revision": "9df3b38af1f57083b9d714aa63637c57",
+    "url": "/Glebhan/static/media/pic3.9df3b38a.jpg"
+  },
+  {
+    "revision": "d4db979b4899ae4a981fb5eea082a9ff",
+    "url": "/Glebhan/static/media/pic4.d4db979b.jpg"
+  },
+  {
+    "revision": "73ca34185ebcaac08d4b7c8ac62f79ed",
+    "url": "/Glebhan/static/media/pic5.73ca3418.jpg"
+  },
+  {
+    "revision": "8e5361abf30cbcfe0dfc64cb7f9e417b",
+    "url": "/Glebhan/static/media/pic6.8e5361ab.jpg"
+  },
+  {
+    "revision": "ca955c3b644c4b9c212f2f52d87908e1",
+    "url": "/Glebhan/static/media/pic7.ca955c3b.jpg"
+  },
+  {
+    "revision": "6e4bd7e772f82f987ca7487ff75e658a",
+    "url": "/Glebhan/static/media/pic8.6e4bd7e7.jpg"
+  },
+  {
+    "revision": "110c73c4e9f055e84695955ec186c59c",
+    "url": "/Glebhan/static/media/pic9.110c73c4.jpg"
+  },
+  {
+    "revision": "6038e8681c306182fda6a2a422b996ff",
+    "url": "/Glebhan/static/media/pic10.6038e868.jpg"
+  },
+  {
+    "revision": "ab67f198d46ee432c0c65c60f291b596",
+    "url": "/Glebhan/static/media/pic11.ab67f198.jpg"
+  },
+  {
+    "revision": "075860e2b4e48f4283b0a5527188c2dd",
+    "url": "/Glebhan/static/media/pic12.075860e2.jpg"
+  },
+  {
+    "revision": "d2cd6983f8ac559d244900574003eae7",
+    "url": "/Glebhan/static/media/pic13.d2cd6983.jpg"
+  },
+  {
+    "revision": "9e5edb38b8d5acfe56562f844714da6e",
+    "url": "/Glebhan/static/media/pic14.9e5edb38.jpg"
+  },
+  {
+    "revision": "d7e6166258993598813ea5e2fdb4d8a8",
+    "url": "/Glebhan/static/media/pic15.d7e61662.jpg"
+  },
+  {
+    "revision": "5d652def49ab57c8ac55b17fb73fe846",
+    "url": "/Glebhan/static/media/pic16.5d652def.jpg"
+  },
+  {
+    "revision": "969e854eed074c879ae1090aa1072a29",
+    "url": "/Glebhan/static/media/pic17.969e854e.jpg"
+  },
+  {
+    "revision": "6363235bb6f13e8409ab0559f80de8a0",
+    "url": "/Glebhan/static/media/pic18.6363235b.jpg"
+  },
+  {
+    "revision": "edb189288c5e59bb8fb054adf1fd8e28",
+    "url": "/Glebhan/index.html"
+  }
+];
