cdf2304736836dd0badab692a2229bdfe787337c
Bijan Parvin <bparvin12@gmail.com>
2019-08-19 21:06:15 -0700

    cover image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,7 +17,7 @@ class Home extends Component {
                 <Carousel controls={false} indicators={false}>
                     <Carousel.Item>
                         <img
-                            className="d-block w-100"
+                            className="d-block w-100 imageCover"
                             src={coverImage}
                             alt="First slide"
                         />
