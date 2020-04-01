5dab849e5c2d77e313953d96dfd05dffdd222daa
BijanParvin <bparvin12@gmail.com>
2020-03-31 21:16:39 -0700

    covid-19 new dates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import Container from 'react-bootstrap/Container';
 // import Image from 'react-bootstrap/Image';
 // import Row from 'react-bootstrap/Row';
 // import Col from 'react-bootstrap/Col'
 // import weddingImage from "../../pictures/wedding.jpg";
 // import liberty from "../../pictures/liberty.jpg"
-import { Carousel, Card } from 'react-bootstrap'
-import coverImage from '../../pictures/glebhanWeddingPhotos/Cover Photo_.jpg'
+import { Carousel, Card } from "react-bootstrap";
+import coverImage from "../../pictures/glebhanWeddingPhotos/Cover Photo_.jpg";
 import "./Home.scss";
 
 class Home extends Component {
     render() {
-
         return (
             <div>
-                <Carousel controls={false} indicators={false} className='customCarousel'>
+                <Carousel
+                    controls={false}
+                    indicators={false}
+                    className="customCarousel"
+                >
                     <Carousel.Item>
                         <img
                             className="d-block w-100 imageCover"
@@ -22,42 +25,97 @@ class Home extends Component {
                             alt="First slide"
                         />
                         <Carousel.Caption>
-                            <Card className='cardHome displayable'>
-                                <div className='writingDiv'>
+                            <Card className="cardHome displayable">
+                                <div className="writingDiv">
                                     <div className="writing1">
                                         Siobhan & Glenn
                                     </div>
                                     <div className="writing2">
-                                        <div>May 2, 2020</div>
+                                        <div>August 28, 2020</div>
                                         <div>San Diego, CA</div>
                                     </div>
                                 </div>
                             </Card>
                         </Carousel.Caption>
-                        <Carousel.Caption className='countdown displayable'>
-                            <div className="countdownTitle">Wedding Countdown</div>
-                            <div data-type="countdown" data-id="1344628" className="tickcounter"><a href="//www.tickcounter.com/countdown/1344601" title="My countdown">My countdown</a></div>
+                        <Carousel.Caption className="countdown displayable">
+                            <div className="countdownTitle">
+                                Wedding Countdown
+                            </div>
+                            <div
+                                data-type="countdown"
+                                data-id="1879946"
+                                className="tickcounter"
+                            >
+                                <a
+                                    href="//www.tickcounter.com/countdown/1879946/my-countdown"
+                                    title="My countdown"
+                                >
+                                    My countdown
+                                </a>
+                                <a
+                                    href="//www.tickcounter.com/"
+                                    title="Countdown"
+                                >
+                                    Countdown
+                                </a>
+                            </div>
+                            {/* <div
+                                data-type="countdown"
+                                data-id="1344628"
+                                className="tickcounter"
+                            >
+                                <a
+                                    href="//www.tickcounter.com/countdown/1879946/my-countdown"
+                                    title="My countdown"
+                                >
+                                    My countdown
+                                </a>
+                            </div> */}
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
                 {/* hidden portions when screen is larger than 480px */}
-                <Card className='cardHome hideable'>
-                    <div className='writingDiv'>
-                        <div className="writing1">
-                            Siobhan & Glenn
-                                    </div>
+                <Card className="cardHome hideable">
+                    <div className="writingDiv">
+                        <div className="writing1">Siobhan & Glenn</div>
                         <div className="writing2">
-                            <div>May 2, 2020</div>
+                            <div>August 28, 2020</div>
                             <div>San Diego, CA</div>
                         </div>
                     </div>
                 </Card>
-                <div className='countdown hideable'>
+                <div className="countdown hideable">
                     <div className="countdownTitle">Wedding Countdown</div>
-                    <div data-type="countdown" data-id="1344628" className="tickcounter"><a href="//www.tickcounter.com/countdown/1344601" title="My countdown">My countdown</a></div>
+                    <div
+                        data-type="countdown"
+                        data-id="1879946"
+                        className="tickcounter"
+                    >
+                        <a
+                            href="//www.tickcounter.com/countdown/1879946/my-countdown"
+                            title="My countdown"
+                        >
+                            My countdown
+                        </a>
+                        <a href="//www.tickcounter.com/" title="Countdown">
+                            Countdown
+                        </a>
+                    </div>
+                    {/* <div
+                        data-type="countdown"
+                        data-id="1344628"
+                        className="tickcounter"
+                    >
+                        <a
+                            href="//www.tickcounter.com/countdown/1879933/my-countdown"
+                            title="My countdown"
+                        >
+                            My countdown
+                        </a>
+                    </div> */}
                 </div>
             </div>
-        )
+        );
     }
 }
 
--- src/components/OurWedding/OurWedding.js
+++ src/components/OurWedding/OurWedding.js
@@ -1,52 +1,65 @@
-import React, { Component } from 'react';
-import { Card, Image, Row, Col } from 'react-bootstrap'
-import photo from '../../pictures/glebhanWeddingPhotos/Cover photo2_.jpg'
+import React, { Component } from "react";
+import { Card, Image, Row, Col } from "react-bootstrap";
+import photo from "../../pictures/glebhanWeddingPhotos/Cover photo2_.jpg";
 
-import "./OurWedding.scss"
+import "./OurWedding.scss";
 
 class OurWedding extends Component {
     render() {
         return (
             <div>
-                <Card className='jumbo'>
+                <Card className="jumbo">
                     <Row>
                         <Col xs={7}>
                             <div className="writingColumnOurWedding">
                                 {/* <Alert className='alertOurWedding'> */}
-                                <div className='alertOurWedding'>Save The Date</div><br></br>
+                                <div className="alertOurWedding">
+                                    Save The Date
+                                </div>
+                                <br></br>
                                 {/* </Alert> */}
                                 {/* <Alert className='alertDetailsOurWedding'> */}
-                                <div className='alertDetailsOurWedding'>
-                                    <div className='headingOurWedding'>Saturday May 02, 2020</div>
+                                <div className="alertDetailsOurWedding">
+                                    <div className="headingOurWedding">
+                                        Friday August 28, 2020
+                                    </div>
                                     {/* Five O'Clock In The Evening */}
-                                </div><br></br><br></br>
+                                </div>
+                                <br></br>
+                                <br></br>
                                 {/* </Alert> */}
                                 {/* <Alert className='alertDetailsOurWedding'> */}
-                                <div className='alertDetailsOurWedding'>
-                                    <div className='headingOurWedding'>Ceremony and Reception</div>
-                                    <div>Liberty Station - Building 177</div>
-                                    <div>2875 Dewey Rd</div>
-                                    <div>San Diego, CA 92106</div>
-                                    <div>(619) 203-5610</div>
+                                <div className="alertDetailsOurWedding">
+                                    <div className="headingOurWedding">
+                                        Ceremony and Reception
+                                    </div>
+                                    {/* place */}
+                                    <div>New Venue Coming Soon!</div>
+                                    {/* address */}
+                                    <div></div>
+                                    <div>San Diego, CA</div>
+                                    {/* phone number */}
+                                    <div></div>
                                     {/* </Alert> */}
-                                </div><br></br>
-                                <div className='alertDetailsOurWedding'>
-                                    <div className='headingOurWedding'>Attire</div>
+                                </div>
+                                <br></br>
+                                <div className="alertDetailsOurWedding">
+                                    <div className="headingOurWedding">
+                                        Attire
+                                    </div>
                                     Semi-Formal
                                 </div>
                             </div>
                         </Col>
-                        <Col xs={5} className='imgColumnOurWedding'>
-                            <div >
-                                <Image src={photo} className='imgOurWedding' />
+                        <Col xs={5} className="imgColumnOurWedding">
+                            <div>
+                                <Image src={photo} className="imgOurWedding" />
                             </div>
                         </Col>
                     </Row>
-
-
                 </Card>
             </div>
-        )
+        );
     }
 }
 
